c43f78e091af954d378991a42ef339eb0b111c69
Strang333 <167582329+Str44ng33@users.noreply.github.com>
2024-08-03 17:02:27 -0400

    Update arquivo.sh

--- arquivo.sh
+++ arquivo.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Define o diretório do projeto
 PROJECT_DIR="/home/strang333/PDFCRACKERV.2byStrang333"
 SCRIPT_NAME="arquivo.py"
 
-# Cria o ambiente virtual se não existir
 if [ ! -d "$PROJECT_DIR/venv" ]; then
     echo "Criando ambiente virtual..."
     python3 -m venv "$PROJECT_DIR/venv"
@@ -12,15 +10,12 @@ else
     echo "Ambiente virtual já existe."
 fi
 
-# Ativa o ambiente virtual
 source "$PROJECT_DIR/venv/bin/activate"
 
-# Instala as dependências
 echo "Instalando dependências..."
 pip install --upgrade pip
 pip install pillow pikepdf tqdm termcolor
 
-# Executa o script Python
 if [ -f "$PROJECT_DIR/$SCRIPT_NAME" ]; then
     echo "Executando o script Python..."
     python3 "$PROJECT_DIR/$SCRIPT_NAME"
@@ -28,7 +23,6 @@ else
     echo "Script Python não encontrado: $SCRIPT_NAME"
 fi
 
-# Instruções adicionais para o usuário
 echo "Dependências instaladas e script executado com sucesso!"
 echo "Para rodar o script no futuro, ative o ambiente virtual e execute o arquivo Python."
 echo "Ativar o ambiente virtual:"
